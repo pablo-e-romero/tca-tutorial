a31eee185f68dda69dfadd436e792641c0e5e659
Pablo Romero <pablo.romero@feverup.com>
2024-03-24 22:36:43 +0100

    Create empty project

---
+++ TCA-tutorial/TCA_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorialApp.swift
+//  TCA-tutorial
+//
+//  Created by Pablo Romero on 24/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
