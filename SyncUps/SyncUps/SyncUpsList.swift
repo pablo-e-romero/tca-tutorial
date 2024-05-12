9f5202e20802b9240713adec28895c1d582f6f8b
Pablo Romero <pablo.romero@feverup.com>
2024-05-12 19:17:49 +0200

    Add SyncUpsListView

--- SyncUps/SyncUps/SyncUpsList.swift
+++ SyncUps/SyncUps/SyncUpsList.swift
@@ -28,3 +28,71 @@ struct SyncUpsList {
         }
     }
 }
+
+struct SyncUpsListView: View {
+    let store: StoreOf<SyncUpsList>
+
+    var body: some View {
+        List {
+            ForEach(store.syncUps) { syncUp in
+                Button {
+
+
+                } label: {
+                    CardView(syncUp: syncUp)
+                }
+                .listRowBackground(syncUp.theme.mainColor)
+            }
+            .onDelete { indexSet in
+                store.send(.onDelete(indexSet))
+            }
+        }
+        .toolbar {
+            Button {
+                store.send(.addSyncUpButtonTapped)
+            } label: {
+                Image(systemName: "plus")
+            }
+        }
+        .navigationTitle("Daily Sync-ups")
+    }
+}
+
+struct CardView: View {
+  let syncUp: SyncUp
+
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(syncUp.title)
+        .font(.headline)
+      Spacer()
+      HStack {
+        Label("\(syncUp.attendees.count)", systemImage: "person.3")
+        Spacer()
+        Label(syncUp.duration.formatted(.units()), systemImage: "clock")
+          .labelStyle(.trailingIcon)
+      }
+      .font(.caption)
+    }
+    .padding()
+    .foregroundColor(syncUp.theme.accentColor)
+  }
+}
+
+#Preview {
+    SyncUpsListView(
+        store: .init(
+            initialState: SyncUpsList.State(syncUps: [
+                .init(
+                    id: SyncUp.ID(),
+                    attendees: [
+                        Attendee(id: Attendee.ID(), name: "Pablo"),
+                        Attendee(id: Attendee.ID(), name: "Gaby"),
+                        Attendee(id: Attendee.ID(), name: "Luati"),
+                    ],
+                    title: "Daily")
+            ]),
+            reducer: { SyncUpsList() }
+        )
+    )
+}
