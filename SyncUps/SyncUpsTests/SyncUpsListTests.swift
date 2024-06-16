a354d002cdec82b3da12dda4e3be3f1a103262d4
Pablo Romero <pablo.romero@feverup.com>
2024-06-16 20:51:30 +0200

    Update SyncUpsListTests

--- SyncUps/SyncUpsTests/SyncUpsListTests.swift
+++ SyncUps/SyncUpsTests/SyncUpsListTests.swift
@@ -20,4 +20,32 @@ final class SyncUpsListTests: XCTestCase {
         }
     }
 
+    @MainActor
+    func testAddSyncUp() async {
+        let store = TestStore(initialState: SyncUpsList.State()) {
+            SyncUpsList()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+
+        store.exhaustivity = .off(showSkippedAssertions: false)
+
+        await store.send(.addSyncUpButtonTapped)
+
+        let editedSyncUp = SyncUp(
+            id: SyncUp.ID(),
+            attendees: [
+                Attendee(id: Attendee.ID(), name: "Blob"),
+                Attendee(id: Attendee.ID(), name: "Blob Jr."),
+            ],
+            title: "Point-Free morning sync"
+        )
+
+        await store.send(\.addSyncUp.binding.syncUp, editedSyncUp)
+
+        await store.send(.confirmAddButtonTapped) {
+            $0.addSyncUp = nil
+            $0.syncUps.append(editedSyncUp)
+        }
+    }
 }
